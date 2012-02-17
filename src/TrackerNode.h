f874166f861166ae3922130f329b7d4c83539947
Grady O'Connell <flipcoder@gmail.com>
2012-02-16 22:22:42 -0800

    Removed old comment

--- src/TrackerNode.h
+++ src/TrackerNode.h
@@ -1,28 +1,3 @@
-
-/**
- * @file	TrackerNode.h
- * @author	Grady O'Connell
- * @url		http://www.prolificaxis.com
- *
- * @section LICENSE
- *
- * For internal use only. Do not redistribute.
- * © 2011 Prolific Axis, LLC
- *
- * @section DESCRIPTION
- *
- * Tracker Node
- * A scene graph node, with the ability to copy other nodes' transformations.
- * A tracker node uses a defined "focus".  What the node copies from the focus
- * is defined by the a FocusFlag.  It can: attach(snap) to the position of, take
- * on the orientation of, face towards, or copy transformations exactly.
- *
- * NOTES: Uses weak_ptr to protect against potential problems with focus destruction
- *
- * FIX: Right now this only thinks in orientational-object and positional-
- * parent space.  Better spacial transformation needs to be added to the Node class.
- */
-
 #ifndef _TRACKERNODE_H
 #define _TRACKERNODE_H
 
