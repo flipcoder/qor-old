730d4ac97cb77f257c2e738ddbbcc99256973fa7
Grady O'Connell <flipcoder@gmail.com>
2012-05-06 00:19:37 -0700

    Improved modding script (eventually will be replaced)

--- mod.sh
+++ mod.sh
@@ -1,2 +1,18 @@
-rm src/game 2>/dev/null
-ln -s mod_$1 src/game
+#!/bin/bash
+# TODO: Eventually this should be a python script that simply renames directories and should not use symbolic links
+
+if [ $# -ne 1 ]; then
+    echo "Swaps between qor mods."
+    echo "Usage: ./mod.sh {mod}"
+    echo "Example: ./mod.sh demo"
+    exit
+fi
+
+if [ -e "mod_$1" ]; then
+    echo "Mod specified does not exist"
+fi
+
+mod="mod_$1"
+
+rm -rf src/game 2>/dev/null
+ln -s $mod src/game
